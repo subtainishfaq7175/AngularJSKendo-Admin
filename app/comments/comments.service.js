6c4df5f6b66f5e7e892b079587fae93f99660bd3
subtainishfaqstudent <subtain>
2016-10-30 15:11:37 +0500

    Comments page ready

---
+++ app/comments/comments.service.js
@@ -0,0 +1,15 @@
+/**
+ * Created by subtainishfaq on 10/30/16.
+ */
+/**
+ * Created by subtainishfaq on 10/13/16.
+ */
+
+angular.module("yapp").factory('commentsService',['$http', function($http){
+
+  var home = {};
+
+
+  return home;
+
+}]);
